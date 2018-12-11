999c123cc9a4c418c437ed7911cea7fea8d63815
alaw1001 <alaw1001@optum.com>
2018-12-11 14:39:14 -0500

    implemented with deque

--- day9/day9.py
+++ day9/day9.py
@@ -1,41 +1,61 @@
-def insert_marble(marble_n, marbles, current_marble_index):
-    marble_n_index = ((current_marble_index + 1) % len(marbles)) + 1
-    marbles.insert(marble_n_index, marble_n)
-    return marbles, marble_n_index
+from collections import deque
+# def insert_marble(marble_n, marbles, current_marble_index):
+#     marble_n_index = ((current_marble_index + 1) % len(marbles)) + 1
+#     marbles.insert(marble_n_index, marble_n)
+#     return marbles, marble_n_index
 
-def get_points(marble_n, marbles, current_marble_index):
-    marble_m_index = current_marble_index - 7
-    if marble_m_index < 0:
-        marble_m_index = len(marbles) + marble_m_index
-    marble_m = marbles.pop(marble_m_index)
-    return marbles, marble_m_index, marble_n + marble_m
+# def get_points(marble_n, marbles, current_marble_index):
+#     marble_m_index = current_marble_index - 7
+#     if marble_m_index < 0:
+#         marble_m_index = len(marbles) + marble_m_index
+#     marble_m = marbles.pop(marble_m_index)
+#     return marbles, marble_m_index, marble_n + marble_m
+
+# def play_marbles(players, max_marble_n):
+#     marbles = [0]
+#     current_marble_index = 0
+#     current_player = 0
+#     player_scores = {i:0 for i in range(players)}
+#     for marble_n in range(1,max_marble_n+1):
+#         if marble_n % 23 == 0:
+#             marbles, current_marble_index, score = get_points(marble_n, marbles, current_marble_index)
+#             player_scores[current_player] += score
+#         else:
+#             marbles, current_marble_index = insert_marble(marble_n, marbles, current_marble_index)
+#         current_player = (current_player + 1) % players
+#     print(sorted([(player_scores[i],i) for i in player_scores])[-1])
+
+def insert_marble(marble_n, marbles):
+    marbles.rotate(-1)
+    marbles.append(marble_n)
+
+def get_points(marble_n, marbles):
+    marbles.rotate(7)
+    score = marbles.pop()
+    marbles.rotate(-1)
+
+    return score + marble_n
 
 def play_marbles(players, max_marble_n):
-    marbles = [0]
-    current_marble_index = 0
+    marbles = deque([0])
     current_player = 0
     player_scores = {i:0 for i in range(players)}
     for marble_n in range(1,max_marble_n+1):
         if marble_n % 23 == 0:
-            marbles, current_marble_index, score = get_points(marble_n, marbles, current_marble_index)
+            score = get_points(marble_n, marbles)
             player_scores[current_player] += score
-            # print(f"{current_player}: {marbles[current_marble_index]}:  {score}")
-            
         else:
-            marbles, current_marble_index = insert_marble(marble_n, marbles, current_marble_index)
-        
-        # print(f"{current_player}: {marbles[current_marble_index]}:  {marbles}")
+            insert_marble(marble_n, marbles)
         current_player = (current_player + 1) % players
-
-
     print(sorted([(player_scores[i],i) for i in player_scores])[-1])
 
+
 if __name__ == '__main__':
-    # play_marbles(9,25)
-    # play_marbles(10,1618)
-    # play_marbles(13,7999)
-    # play_marbles(17,1104)
-    # play_marbles(21,6111)
-    # play_marbles(30,5807)
+    play_marbles(9,25)
+    play_marbles(10,1618)
+    play_marbles(13,7999)
+    play_marbles(17,1104)
+    play_marbles(21,6111)
+    play_marbles(30,5807)
     play_marbles(403,71920)
-    # play_marbles(403,71920*100)
+    play_marbles(403,71920*100)
